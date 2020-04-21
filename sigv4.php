8ebdfc7c82b55e9ded263baee15fe75fa80de40f
Ken <ken@KensArch.localdomain>
2020-04-21 12:40:32 +0100

    Initial commit

---
+++ sigv4.php
@@ -0,0 +1,143 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>AWS Experiments</title>
+
+<script src="https://sdk.amazonaws.com/js/aws-sdk-2.632.0.min.js"></script>
+</head>
+<body>
+	<header>
+		<div id="right">
+				<h1>AWS Signature v4</h1>
+		</div>
+	</header>
+<?php
+	define("IOT_ENDPOINT","https://a3v1d70hhtwfc0-ats.iot.eu-west-2.amazonaws.com");
+	define("IOT_PORT","443");
+	function cmp($a, $b) {
+		return strcasecmp($a, $b);
+	}
+	function sign($key,$msg){
+		return hash_hmac('sha256',$msg,hex2bin($key));
+	}
+	function Send($path,$payload){
+		//echo "Send\n";
+		//echo IOT_ENDPOINT;
+		$server=IOT_ENDPOINT.":".IOT_PORT;
+		$requrl=$server.$path."?qos=1";
+		$region="eu-west-2";
+		$method="";
+
+		$ksecret="";
+		$keyid='';
+		$lines = file('../../ssl/creds/kennyams.cred');
+		foreach ($lines as $no => $line){
+			$crd=explode(' = ',$line);
+			if($crd[0]=='aws_access_key_id'){
+				$keyid=trim($crd[1]);
+			}else{
+				$ksecret=trim($crd[1]);
+			}
+		}
+
+		$method="POST";
+		$datetime=new DateTime("now");
+		$tz=new DateTimeZone("UTC");
+		date_timezone_set($datetime,$tz);
+		$dt=$datetime->format("Ymd\THis\Z");
+		$d=$datetime->format("Ymd");
+		$service="iotdata";
+
+		$parsedurl=parse_url($requrl);
+		$request=$method."\n";
+
+		if($parsedurl['path']=='/'){
+			$request.="/\n";
+		}else{
+			$request.=$parsedurl['path']."\n";
+		}
+		$request.=utf8_encode($parsedurl['query'])."\n";
+
+		$headers='';
+		$signedHeaders='';
+		$headersToBSigned = array(
+			'host'=>$parsedurl['host'],
+			'X-Amz-Date'=>$dt
+		);
+
+		uksort($headersToBSigned,'cmp');
+
+
+		foreach($headersToBSigned as $key => $value){
+			$headers.=strtolower($key).":".$value."\n";
+			$signedHeaders.=strtolower($key).";";
+		} 
+		//echo "HEADERS=>".$headers."--------------------------\n";
+		$signedHeaders=substr($signedHeaders,0,-1);
+		//echo "SIGNEDHEADERS=>".$signedHeaders."--------------------------\n";
+
+		$request.=$headers."\n".$signedHeaders."\n";
+
+		$request.=hash('sha256',$payload);
+
+		//echo "REQUEST=>\n".$request."--------------------\n";
+		$hrequest=hash('sha256',$request);
+
+		//echo "HREQUEST for step 2=>\n".$hrequest."---------------------\n\n";
+
+
+		$credentialsScope=$d."/".$region."/".$service."/aws4_request";
+		$stringtosign = "AWS4-HMAC-SHA256\n";
+		$stringtosign.= $dt."\n";
+		$stringtosign.= $credentialsScope."\n";
+		//$stringtosign.= $d."/".$region."/iot/aws4_request\n";
+		$stringtosign.= $hrequest;
+		//echo "STRING TO SIGN=>\n".$stringtosign."\n----------------\n\n";
+		//echo "STEP3\n";
+		$kdate = sign(bin2hex("AWS4".$ksecret),$d);
+		$kregion = sign($kdate,$region);
+		$kservice = sign($kregion,$service);
+		$ksigning = sign($kservice ,"aws4_request");
+		//echo "SIGNING KEY=>\n".$ksigning."\n";
+
+		$signed=sign($ksigning,$stringtosign);
+		//echo "signed\n";
+		//echo $signed."\n";
+
+		$headers=array();
+		foreach($headersToBSigned as $key => $value){
+			array_push($headers,$key.":".$value);
+		}
+		//print_r($signedHeaders."\n");
+		array_push($headers,'Content-Length:'.strlen($payload));
+		$authorization='Authorization:'."AWS4-HMAC-SHA256 Credential=".$keyid."/".$credentialsScope.",SignedHeaders=".$signedHeaders.",Signature=".$signed;
+		//echo "AUTHORIZATION=>\n".$authorization."\n";
+		array_push($headers,$authorization);
+		//print_r($headers);
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, $requrl);
+		if( $method=="POST"){
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+		}
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+		$out = curl_exec($ch);
+		//print_r(curl_getinfo($ch, CURLINFO_HTTP_CODE));
+		if($out === False){
+			print_r(curl_error($ch));
+			echo "NOK";
+		}else{
+			//echo "OK";
+			//print_r($out);
+		}
+		//echo "</textarea>";
+		curl_close($ch);
+		return $out;
+	}
+?>
+</body>
+</html>
